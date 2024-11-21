081a4a66b49237ac1f84149c355636560a25af44
matheusbarcc <matheusbarcon@gmail.com>
2024-11-21 00:58:02 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <Button>Clicar</Button>
+    </div>
+  )
+}
