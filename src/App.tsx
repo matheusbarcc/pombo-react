13f66723180a4c83f8565de6ecfcfa86b039b87c
matheusbarcc <matheusbarcon@gmail.com>
2024-11-21 15:10:30 -0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pombo" />
-      <Toaster richColors />
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <ThemeProvider storageKey="pombo-theme" defaultTheme="light">
+        <Helmet titleTemplate="%s | pombo" />
+        <Toaster richColors />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
