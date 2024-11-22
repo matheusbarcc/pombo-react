584851188b2024b047bfdd6403fc0350acf3e78a
matheusbarcc <matheusbarcon@gmail.com>
2024-11-21 22:08:02 -0300

    style: add feed page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -8,7 +8,8 @@ import { ThemeToggle } from '@/components/theme/theme-toggle'
 export function AppLayout() {
   return (
     <div className="grid min-h-screen grid-cols-[1fr_2fr_1fr] px-52 antialiased gap-4">
-      <div className="flex flex-col border-r border-foreground/15 p-7 gap-6">
+      {/* Left Column */}
+      <div className="flex flex-col border-r border-foreground/15 p-7 gap-6 scroll sticky top-0 h-screen">
         <div className="flex justify-between items-center">
           <div className="flex gap-3 items-center">
             <Bird className="h-8 w-8" />
@@ -18,10 +19,14 @@ export function AppLayout() {
         </div>
         <MiniProfileCard />
       </div>
-      <div>
+
+      {/* Center Column */}
+      <div className="overflow-y-auto scrollbar-hide">
         <Outlet />
       </div>
-      <div className="flex flex-col border-l border-foreground/15 p-7 gap-3">
+
+      {/* Right Column */}
+      <div className="flex flex-col border-l border-foreground/15 p-7 gap-3 sticky top-0 h-screen">
         <SearchAndFilters />
       </div>
     </div>
