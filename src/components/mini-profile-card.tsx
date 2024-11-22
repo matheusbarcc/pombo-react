89e9d1b0a175dd17258391fb8f3654683f36f2b9
matheusbarcc <matheusbarcon@gmail.com>
2024-11-21 21:40:58 -0300

    style: add app layout

---
+++ src/components/mini-profile-card.tsx
@@ -0,0 +1,41 @@
+import { LogOut, PencilLine } from 'lucide-react'
+
+import profilePic from '../assets/mike.jpg'
+import { Button } from './ui/button'
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Separator } from './ui/separator'
+
+export function MiniProfileCard() {
+  return (
+    <Card className="flex flex-col items-center">
+      <CardHeader className="flex flex-col items-center gap-3">
+        <img
+          src={profilePic}
+          alt=""
+          className="w-16 h-1w-16 rounded-lg outline outline-offset-2 outline-primary"
+        />
+        <div className="flex flex-col">
+          <CardTitle className="text-lg">Mike Ehrmantraut</CardTitle>
+          <CardDescription>waltuh@example.com</CardDescription>
+        </div>
+      </CardHeader>
+      <Separator />
+      <CardFooter className="flex flex-col gap-2 w-full py-3">
+        <Button className="w-full">
+          <PencilLine />
+          <span>Editar Perfil</span>
+        </Button>
+        <Button variant="secondary" className="w-full  text-rose-500">
+          <LogOut />
+          <span className="">Sair</span>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,29 @@
+import { Bird } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
+
+import { MiniProfileCard } from '@/components/mini-profile-card'
+import { SearchAndFilters } from '@/components/search-and-filters'
+import { ThemeToggle } from '@/components/theme/theme-toggle'
 
 export function AppLayout() {
   return (
-    <div>
-      App layout
-      <Outlet />
+    <div className="grid min-h-screen grid-cols-[1fr_2fr_1fr] px-52 antialiased gap-4">
+      <div className="flex flex-col border-r border-foreground/15 p-7 gap-6">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-3 items-center">
+            <Bird className="h-8 w-8" />
+            <span className="font-bold text-2xl">pombo</span>
+          </div>
+          <ThemeToggle />
+        </div>
+        <MiniProfileCard />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <div className="flex flex-col border-l border-foreground/15 p-7 gap-3">
+        <SearchAndFilters />
+      </div>
     </div>
   )
 }
